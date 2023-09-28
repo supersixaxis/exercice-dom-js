2f1d1e01d6deb1a736820702a9dabd7dcb983959
supersixaxis <appert.nathan@outlook.fr>
2023-09-28 16:23:11 +0200

    exo 3

--- index.js
+++ index.js
@@ -20,16 +20,16 @@ numberInput.addEventListener('input', (e) => {
 square.style.backgroundColor = 'blue';
 square.style.width = '100px';
 square.style.height = '100px';
-
+square.style.position = "absolute"
 
 let isMovedDown = false;
 let isMovedRight = false;
 let isMovedTop = false;
-
+let isMovedLeft = false;
 moveSquare.addEventListener('click', (e) => {
     if (!isMovedDown){
         square.style.position = "absolute"
-        square.style.bottom = "400px"
+        square.style.bottom = "200px"
         isMovedDown = true
     } else if (!isMovedRight){
         square.style.position = "absolute"
@@ -37,15 +37,19 @@ moveSquare.addEventListener('click', (e) => {
         isMovedRight = true
     } else if (!isMovedTop){
         square.style.position = "absolute"
-        square.style.top = "300px"
+        square.style.top = "400px"
         isMovedTop = true
-    } else if (isMovedDown === true && isMovedRight === true && isMovedTop === true){
+    }else if (!isMovedLeft){
+        square.style.left = "10px"
+        isMovedLeft = true
+        }else if (isMovedDown === true && isMovedRight === true && isMovedTop === true && isMovedLeft === true){
          square.style.position = "absolute"
          square.style.left = "initial";
          square.style.top = "initial";
          isMovedDown = false;
          isMovedRight = false;
          isMovedTop = false;
+         isMovedLeft = false;
     }
 
 })
